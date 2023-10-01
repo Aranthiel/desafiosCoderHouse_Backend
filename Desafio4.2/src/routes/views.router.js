43fd77e0bbf22a77a446625606c91d8a0c2ac555
Aranthiël (Natalia Moroni) <nmoronidalmasso@gmail.com>
2023-10-01 19:03:07 -0300

    home.handlebars creada, pero no muestra productos

--- Desafio4.2/src/routes/views.router.js
+++ Desafio4.2/src/routes/views.router.js
@@ -6,7 +6,7 @@ const router = Router();
 
 //endpopint GET para obtener TODOS LOS PRODUCTOS
 router.get('/', (req, res)=>{
-    res.render("websocket");
+    res.render("home");
     //res.render("chat");
 
 }); 
